d573c652a0efb33199e7f45ca2d30118903aef0c
beyondakun <beyond_akun@163.com>
2016-11-30 20:00:54 +0800

    mod Users v1.0

--- app/User.php
+++ app/User.php
@@ -3,8 +3,38 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Session;
 
 class User extends Model
 {
-    //
+
+    //是否已经登陆
+    public static function isLogged($id)
+    {
+        return Session::get('id') == $id? true : false;
+    }
+
+
+    //是否已经注册
+    public static function isRegisted($id)
+    {
+        return User::find($id);
+    }
+
+
+    //注册
+    public static function signup(Request $request)
+    {
+        $user = new User();
+        $user->password = Hash::make($request->input('password')); //加密
+        $user->name = $request->input('user_name');
+
+        //保存模型
+        if ($user->save())
+            return true;
+        else
+            return false;
+    }
+
+
 }
