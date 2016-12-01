6167c8537fd5bc8b81e3cb9a3a4f5581b0d9ab02
beyondakun <beyond_akun@163.com>
2016-12-01 09:01:37 +0800

    mod Users v1.011

--- app/User.php
+++ app/User.php
@@ -3,38 +3,68 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Model
 {
 
     //是否已经登陆
-    public static function isLogged($id)
+    public static function isLogged($user_name)
     {
-        return Session::get('id') == $id? true : false;
+        return Session::get('user_name') == $id? true : false;
     }
 
 
     //是否已经注册
-    public static function isRegisted($id)
+    public static function isRegisted($user_name)
     {
-        return User::find($id);
+        return User::where('name', $user_name)->first() ?: false ;
     }
 
 
-    //注册
+    //注册成功 返回true
     public static function signup(Request $request)
     {
+
         $user = new User();
-        $user->password = Hash::make($request->input('password')); //加密
-        $user->name = $request->input('user_name');
+        $user->password = Hash::make($request->input('password'));      //加密
+        $user->user_name = $request->get('user_name');
 
         //保存模型
-        if ($user->save())
-            return true;
-        else
-            return false;
+        return $user->save() ? true : false;
+
     }
 
+
+    //成功登陆 返回true
+    public static function login(Request $request)
+    {
+        $user_name = $request->get('user_name');
+
+        if (self::isRegisted( $user_name )) {
+            $request->session()->put('user_name', $user_name);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    //成功登出，返回false
+    public static function logout(Request $request)
+    {
+        $user_name = $request->get('user_name');
+
+        if ( session('user_name') == $user_name )
+        {
+            $request->session()->forget('user_name');
+            return true;
+        }
+
+        return false;
+    }
 
 }
