c9681dc2deb63be4bec9d72dc9076cdedf678051
beyondakun <beyond_akun@163.com>
2016-12-01 08:40:06 +0800

    mod Users v1.01

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
-    //
+
+    //注册控制器
+    public function signup(Request $request)
+    {
+        return User::signup($request) ? $request->get('user_name') : 'failed saving model:user';
+    }
+
 }
